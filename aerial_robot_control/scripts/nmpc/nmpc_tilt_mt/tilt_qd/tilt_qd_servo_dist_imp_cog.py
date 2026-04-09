6311af46ced07255174b3e2c397c3a653998d085
Jinjie Li <45286479+Li-Jinjie@users.noreply.github.com>
2026-04-09 13:33:30 +0900

    [py][imp]: add a pure cog impedance func to debug

---
+++ aerial_robot_control/scripts/nmpc/nmpc_tilt_mt/tilt_qd/tilt_qd_servo_dist_imp_cog.py
@@ -0,0 +1,257 @@
+#!/usr/bin/env python
+# -*- encoding: ascii -*-
+import numpy as np
+import casadi as ca
+from .qd_nmpc_base import QDNMPCBase
+from .fake_sensor import FakeSensor
+from . import phys_param_beetle_omni as phys_omni
+
+
+class NMPCTiltQdServoImpedanceCoG(QDNMPCBase):
+    """
+    Controller Name: Tiltable Quadrotor NMPC including Servo Model as well as CoG Disturbance and Impedance Cost function
+    The controller itself is constructed in base class. This file is used to define the properties
+    of the controller, specifically, the weights and cost function for the acados solver.
+    The output of the controller is the thrust and servo angle command for each rotor.
+    """
+
+    def __init__(self, build: bool = True, phys=phys_omni):
+        # Model name
+        self.model_name = "tilt_qd_servo_dist_imp_cog_mdl"
+        self.phys = phys
+
+        self.tilt = True
+        self.include_servo_model = True
+        self.include_servo_derivative = False
+        self.include_thrust_model = False  # TODO extend to include_thrust_derivative
+        self.include_cog_dist_model = True
+        self.include_cog_dist_parameter = True
+        self.include_impedance = True
+
+        # Read parameters from configuration file in the robot's package
+        self.read_params("controller", "nmpc", "beetle_omni", "BeetleNMPCFullServoImp.yaml")
+
+        # Create acados model & solver and generate c code
+        super().__init__(build)
+
+        # Necessary for simulation environment
+        self.fake_sensor = FakeSensor(self.include_servo_model, self.include_thrust_model, self.include_cog_dist_model)
+
+    def get_cost_function(self, lin_acc_w=None, ang_acc_b=None):
+        # fmt: off
+        # Cost function
+        # see https://docs.acados.org/python_interface/#acados_template.acados_ocp_cost.AcadosOcpCost for details
+        # NONLINEAR_LS = error^T @ Q @ error; error = y - y_ref
+        # qe = qr^* multiply q
+        qe_w, qe_x, qe_y, qe_z = self._quaternion_multiply(self.qwr, -self.qxr, -self.qyr, -self.qzr,
+                                                           self.qw, self.qx, self.qy, self.qz,)
+
+        # qe_x, qe_y, qe_z = quaternion_rotvec_casadi(qe_w, qe_x, qe_y, qe_z)
+
+        state_y = ca.vertcat(
+            self.p,
+            self.v,
+            self.qwr,
+            qe_x + self.qxr,
+            qe_y + self.qyr,
+            qe_z + self.qzr,
+            self.w,
+            self.a_s,
+            # ca.times(lin_acc_w, self.mp) - self.fds_w
+            - self.fds_w,
+            # ca.times(ang_acc_b, self.mq) - rot_tb @ (self.tau_ds_b - skew_ee_p @ rot_bw @ self.fds_w)
+            - self.tau_ds_b,
+        )
+
+        state_y_e = ca.vertcat(
+            self.p,
+            self.v,
+            self.qwr,
+            qe_x + self.qxr,
+            qe_y + self.qyr,
+            qe_z + self.qzr,
+            self.w,
+            self.a_s,
+            - self.fds_w,
+            - self.tau_ds_b,
+        )
+
+        control_y = ca.vertcat(
+            self.ft_c,
+            self.a_c - self.a_s  # a_c_ref must be zero!
+        )
+
+        return state_y, state_y_e, control_y
+        # fmt: on
+
+    def get_weights(self):
+        # Define Weights
+        Q = np.diag(
+            [
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                self.params["Qa"],
+                self.params["Qa"],
+                self.params["Qa"],
+                self.params["Qa"],
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+            ]
+        )
+
+        pM_imp = np.eye(3)
+        pD_imp = np.diag([self.params["Qv_xy"], self.params["Qv_xy"], self.params["Qv_z"]])
+        pK_imp = np.diag([self.params["Qp_xy"], self.params["Qp_xy"], self.params["Qp_z"]])
+
+        oM_imp = np.eye(3)
+        oD_imp = np.diag([self.params["Qw_xy"], self.params["Qw_xy"], self.params["Qw_z"]])
+        oK_imp = np.diag([self.params["Qq_xy"], self.params["Qq_xy"], self.params["Qq_z"]])
+
+        enlarge_factor = self.params["enlarge_factor"]
+
+        p_weight = np.concatenate([pK_imp, pD_imp, pM_imp])
+        p_weight_mtx = np.dot(p_weight * enlarge_factor, p_weight.T * enlarge_factor)
+        Q[0:6, 0:6] = p_weight_mtx[0:6, 0:6]
+        Q[17:20, 17:20] = p_weight_mtx[6:9, 6:9]
+        Q[0:6, 17:20] = p_weight_mtx[0:6, 6:9]
+        Q[17:20, 0:6] = p_weight_mtx[6:9, 0:6]
+
+        o_weight = np.concatenate([oK_imp, oD_imp, oM_imp])
+        o_weight_mtx = np.dot(o_weight * enlarge_factor, o_weight.T * enlarge_factor)
+        Q[7:13, 7:13] = o_weight_mtx[0:6, 0:6]
+        Q[20:23, 20:23] = o_weight_mtx[6:9, 6:9]
+        Q[7:13, 20:23] = o_weight_mtx[0:6, 6:9]
+        Q[20:23, 7:13] = o_weight_mtx[6:9, 0:6]
+
+        print("Q: \n", Q)
+
+        R = np.diag(
+            [
+                self.params["Rt"],
+                self.params["Rt"],
+                self.params["Rt"],
+                self.params["Rt"],
+                self.params["Rac_d"],
+                self.params["Rac_d"],
+                self.params["Rac_d"],
+                self.params["Rac_d"],
+            ]
+        )
+        print("R: \n", R)
+
+        return Q, R
+
+    def get_reference(self, target_xyz, target_qwxyz, ft_ref, a_ref):
+        """
+        Assemble reference trajectory from target pose and reference control values.
+        Gets called from reference generator class.
+        Note: The definition of the reference is closely linked to the definition of the cost function.
+        Therefore, this is explicitly stated in each controller file to increase comprehensiveness.
+
+        :param target_xyz: Target position
+        :param target_qwxy: Target quarternions
+        :param ft_ref: Target thrust
+        :param a_ref: Target servo angles
+        :return xr: Reference for the state x
+        :return ur: Reference for the input u
+        """
+        # Get dimensions
+        ocp = self.get_ocp()
+        nn = ocp.solver_options.N_horizon
+        nx = ocp.dims.nx
+        nu = ocp.dims.nu
+
+        # Assemble state reference
+        xr = np.zeros([nn + 1, nx])
+        xr[:, 0] = target_xyz[0]  # x
+        xr[:, 1] = target_xyz[1]  # y
+        xr[:, 2] = target_xyz[2]  # z
+        # No reference for vx, vy, vz (idx: 3, 4, 5)
+        xr[:, 6] = target_qwxyz[0]  # qx
+        xr[:, 7] = target_qwxyz[1]  # qx
+        xr[:, 8] = target_qwxyz[2]  # qy
+        xr[:, 9] = target_qwxyz[3]  # qz
+        # No reference for wx, wy, wz (idx: 10, 11, 12)
+        xr[:, 13] = a_ref[0]
+        xr[:, 14] = a_ref[1]
+        xr[:, 15] = a_ref[2]
+        xr[:, 16] = a_ref[3]
+
+        # Assemble input reference
+        # Note: Reference has to be zero if variable is included as state in cost function!
+        ur = np.zeros([nn, nu])
+        ur[:, 0] = ft_ref[0]
+        ur[:, 1] = ft_ref[1]
+        ur[:, 2] = ft_ref[2]
+        ur[:, 3] = ft_ref[3]
+
+        return xr, ur
+
+
+def quaternion_log_casadi(qe_w, qe_x, qe_y, qe_z, eps=1e-8):
+    """
+    Quaternion logarithm for unit quaternion qe = [w, x, y, z].
+
+    Input:
+        qe_w, qe_x, qe_y, qe_z : CasADi scalar symbols
+
+    Output:
+        e_log_x, e_log_y, e_log_z : CasADi scalar symbols
+        where log(qe) = [e_log_x, e_log_y, e_log_z]^T
+    """
+    # normalize
+    q_norm = ca.sqrt(qe_w**2 + qe_x**2 + qe_y**2 + qe_z**2)
+    w = qe_w / q_norm
+    x = qe_x / q_norm
+    y = qe_y / q_norm
+    z = qe_z / q_norm
+
+    # shortest-path representation: enforce w >= 0
+    sign = ca.if_else(w < 0, -1.0, 1.0)
+    w = sign * w
+    x = sign * x
+    y = sign * y
+    z = sign * z
+
+    v_norm = ca.sqrt(x**2 + y**2 + z**2)
+    angle_half = ca.atan2(v_norm, w)
+
+    scale = ca.if_else(v_norm > eps, angle_half / v_norm, 0.0)
+
+    e_log_x = scale * x
+    e_log_y = scale * y
+    e_log_z = scale * z
+
+    return e_log_x, e_log_y, e_log_z
+
+
+def quaternion_rotvec_casadi(qe_w, qe_x, qe_y, qe_z, eps=1e-8):
+    """
+    Return rotation vector error e_R = 2 * log(qe) = theta * u
+    """
+    e_log_x, e_log_y, e_log_z = quaternion_log_casadi(qe_w, qe_x, qe_y, qe_z, eps)
+
+    e_rot_x = 2.0 * e_log_x
+    e_rot_y = 2.0 * e_log_y
+    e_rot_z = 2.0 * e_log_z
+
+    return e_rot_x, e_rot_y, e_rot_z
+
+
+if __name__ == "__main__":
+    print("Please run the gen_nmpc_code.py in the nmpc folder to generate the code for this controller.")
