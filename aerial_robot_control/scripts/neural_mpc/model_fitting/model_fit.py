a8101cc3953dcedebd4dcbf02cef34aa90e54a2c
Johannes Kübel <johannes.kubel@gmail.com>
2026-04-27 18:48:40 +0900

    [wandb] Script to setup experiments in wandb

--- aerial_robot_control/scripts/neural_mpc/model_fitting/model_fit.py
+++ aerial_robot_control/scripts/neural_mpc/model_fitting/model_fit.py
@@ -34,7 +34,37 @@ T_samp = neural_mpc.T_samp
 T_step = neural_mpc.T_step
 N = neural_mpc.N
 
+# import random
 
+# import wandb
+
+# # Start a new wandb run to track this script.
+# run = wandb.init(
+#     # Set the wandb entity where your project will be logged (generally your team name).
+#     entity="johannes-kuebel-d",
+#     # Set the wandb project where this run will be logged.
+#     project="my-awesome-project",
+#     # Track hyperparameters and run metadata.
+#     config={
+#         "learning_rate": 0.02,
+#         "architecture": "CNN",
+#         "dataset": "CIFAR-100",
+#         "epochs": 10,
+#     },
+# )
+
+# # Simulate training.
+# epochs = 10
+# offset = random.random() / 5
+# for epoch in range(2, epochs):
+#     acc = 1 - 2**-epoch - random.random() / epoch - offset
+#     loss = 2**-epoch + random.random() / epoch + offset
+
+#     # Log metrics to wandb.
+#     run.log({"acc": acc, "loss": loss})
+
+# # Finish the run and upload any remaining data.
+# run.finish()
 
 def main(test: bool = False, plot: bool = False, save: bool = True):
     device = get_device()
